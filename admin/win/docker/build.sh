951fb895fa9403005e4d43b9add0c1e36717c56e
osboxes <osboxes@localhost>
2016-12-09 16:39:07 +0000

    Update build.sh

--- admin/win/docker/build.sh
+++ admin/win/docker/build.sh
@@ -5,15 +5,12 @@ if [ $# -lt 1 ]; then
   exit
 fi
 
-useradd user -u ${2:-1000}
-su - user << EOF
-  cd /home/user/$1
-  rm -rf build-win32
-  mkdir build-win32
-  cd build-win32
-  ../admin/win/download_runtimes.sh
-  cmake .. -DCMAKE_TOOLCHAIN_FILE=../admin/win/Toolchain-mingw32-openSUSE.cmake -DWITH_CRASHREPORTER=ON
-  make -j4
-  make package
-  ctest .
-EOF
+cd /home/user/$1
+rm -rf build-win32
+mkdir build-win32
+cd build-win32
+../admin/win/download_runtimes.sh
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../admin/win/Toolchain-mingw32-openSUSE.cmake -DWITH_CRASHREPORTER=ON
+make -j4
+make package
+ctest .
