b388f5761f9decceda97c60dea76a8f723a3a985
osboxes <osboxes@localhost>
2016-12-10 15:47:16 +0000

    Update OWNCLOUD.make: change Application name and domain

--- OWNCLOUD.cmake
+++ OWNCLOUD.cmake
@@ -1,8 +1,8 @@
-set( APPLICATION_NAME       "ownCloud" )
-set( APPLICATION_EXECUTABLE "owncloud" )
-set( APPLICATION_DOMAIN     "owncloud.com" )
-set( APPLICATION_VENDOR     "ownCloud" )
-set( APPLICATION_UPDATE_URL "https://updates.owncloud.com/client/" CACHE string "URL for updater" )
+set( APPLICATION_NAME       "ProjectKit Drive" )
+set( APPLICATION_EXECUTABLE "pkdrive" )
+set( APPLICATION_DOMAIN     "projectkit.net" )
+set( APPLICATION_VENDOR     "ProjectKit" )
+set( APPLICATION_UPDATE_URL "https://updates.projectkit.net/owncloud-client/" CACHE string "URL for updater" )
 
 set( THEME_CLASS            "ownCloudTheme" )
 set( APPLICATION_REV_DOMAIN "com.owncloud.desktopclient" )
@@ -14,5 +14,5 @@ set( MAC_INSTALLER_BACKGROUND_FILE "${CMAKE_SOURCE_DIR}/admin/osx/installer-back
 # set( APPLICATION_LICENSE    "${OEM_THEME_DIR}/license.txt )
 
 option( WITH_CRASHREPORTER "Build crashreporter" OFF )
-set( CRASHREPORTER_SUBMIT_URL "https://crash-reports.owncloud.com/submit" CACHE string "URL for crash reporter" )
+set( CRASHREPORTER_SUBMIT_URL "https://crash-reports.projectkit.net/submit" CACHE string "URL for crash reporter" )
 set( CRASHREPORTER_ICON ":/owncloud-icon.png" )
